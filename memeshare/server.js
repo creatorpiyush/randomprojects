e2418794ab27a690b50efbd99d89b91eaea31882
creatorpiyush <piyushanand.1221@gmail.com>
2020-08-25 17:21:02 +0530

    bootstrap packaged added

--- memeshare/server.js
+++ memeshare/server.js
@@ -1,21 +1,35 @@
+// * npm packages
 const express = require("express");
 const session = require("express-session");
 
-const port = process.env.PORT || 5555;
+// ****
 
 const app = express();
 
+// * routes('./src/routes')
+const indexroute = require("./src/routes/index");
+
+// *****
+
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
-app.use("/", express.static(__dirname + "./src/public"));
+app.use("/", express.static(__dirname + "/src/public"));
 app.use(express.urlencoded({ urlencoded: true }));
 app.use(express.json());
 
+// * express-session
 
-app.get("/", (req, res) => {
-  res.render("index", { doc: `Meme Page` });
-});
+// *******
+
+// * dynamics
+app.get("/", indexroute);
+
+// *******
+
+// * port
+const port = process.env.PORT || 5555;
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
+// *******
