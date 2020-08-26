bf736414d0d03c2bf7c566c052ba4bb9ce7b2f9d
creatorpiyush <piyushanand.1221@gmail.com>
2020-08-27 00:39:20 +0530

    changes card.js not 100% working for onclick

--- memeshare/src/public/app/cards.js
+++ memeshare/src/public/app/cards.js
@@ -1,7 +1,40 @@
-let count = 0;
+function loadProducts() {
+  $.get("/", (product) => {
+    for (let i = 0; i < 10; i++) {
+      $("#posts-container").append(
+        $(`<div class="card-image">
+                <img src="images/sample-1.jpg">
+                <span class="card-title">Card Title</span>
+                <span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light" id='likebtn'> <i class="material-icons"
+                            id="like" style="font-size: 35px;">favorite_border</i></a>
+                </span>
+            </div>
+`)
+      );
+    }
+  });
+}
 
-document.getElementById("like").ondblclick = () => {
-  count++;
-};
+$("#likebtn").dblclick(function () {
+  // $("#like").append(`favorite`);
+  alert("bye");
+});
 
-console.log(count);
+
+
+// window.onload = function () {
+//   document.getElementById("likebtn").onclick = function () {
+//     getElementById("like").innerHTML = "favorite";
+//   };
+// };
+
+// ondblclick="
+
+// let count = 0;
+
+// document.getElementById("like").onclick = () => {
+//   count++;
+// };
+
+// console.log(count);
