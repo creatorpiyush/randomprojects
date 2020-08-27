9f0421ea8c4d9acb6c279efdc747fab8ad2b93a7
creatorpiyush <piyushanand.1221@gmail.com>
2020-08-27 18:23:08 +0530

    front-end part ready backend in making

--- memeshare/src/public/app/cards.js
+++ memeshare/src/public/app/cards.js
@@ -3,9 +3,9 @@ function loadProducts() {
     for (let i = 0; i < 10; i++) {
       $("#posts-container").append(
         $(`<div class="card-image">
-                <img src="../images/51310067.jpeg">
+                <img src="">
                 <span class="card-title">Card Title</span>
-                <a href="../images/51310067.jpeg" class="btn-floating halfway-fab waves-effect waves-light blue" style="font-size: 35px;" download="File_name"> 
+                <a href="" class="btn-floating halfway-fab waves-effect waves-light blue" style="font-size: 35px;" download="File_name"> 
                 <i class="material-icons">get_app</i></a>
             </div>
 `)
@@ -19,14 +19,6 @@ function clinked() {
   document.getElementById("liked").innerHTML = "favorite";
   console.log("btn clinked");
 }
-
-// window.onload = function () {
-//   document.getElementById("likebtn").onclick = function () {
-//     getElementById("like").innerHTML = "favorite";
-//   };
-// };
-
-// ondblclick="
 
 // let count = 0;
 
