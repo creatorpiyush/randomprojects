cc74a4148e4028027ffc3802b4285cd171f91e5d
creatorpiyush <piyushanand.1221@gmail.com>
2020-08-27 21:25:41 +0530

    db created

---
+++ memeshare/src/db/db.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const db_url = `mongodb://localhost:27017/memepage_db`;
+
+mongoose.connect(
+  db_url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err, info) => {
+    if (err) console.log(err);
+    else {
+      console.log(`DB connect Sucess...`);
+      console.log(info);
+    }
+  }
+);
+
+const memeSchema = mongoose.Schema({
+  memeid: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  memeimg: {
+    type: String,
+    require: true,
+  },
+  memetitle: {
+    type: String,
+    require: true,
+  },
+  publishDate: Date,
+});
+
+mongoose.model("memes", memeSchema);
+
+module.exports = mongoose.model("memes");
