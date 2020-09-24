3fa826b984f33672670914ca8879db55bd5e3f85
Piyush Anand <piyushanand.1221@gmail.com>
2020-09-24 19:18:49 +0530

    adding some css

---
+++ todolist/public/script.js
@@ -0,0 +1,20 @@
+// const form = document.querySelector("form");
+
+// // console.log(form);
+
+// // let tasks = task.innerText;
+
+// const params = new URLSearchParams(window.location.search);
+
+// let tasks = [];
+
+// for (const param of params) {
+//   console.log(param[1]);
+//   tasks.push(param[1]);
+// }
+
+// tasks.forEach((task) => {
+//   console.log(task);
+// });
+
+// console.log();
